ff52b00ddce847efdbf52c099f1b327e68f0c812
Jesse Hixson <jesse.hixson@ahead.com>
2024-03-14 13:29:55 -0400

    Organize tests

--- test/nested_lines_test.exs
+++ test/nested_lines_test.exs
@@ -2,37 +2,43 @@ defmodule NestedLinesTest do
   use ExUnit.Case
   doctest NestedLines
 
-  test "nil values return [[1]]" do
-    input1 = NestedLines.new!([nil])
-    assert %NestedLines{lines: [[1]]} = input1
+  describe "nil values" do
+    test "nil values return [[1]]" do
+      input1 = NestedLines.new!([nil])
+      assert %NestedLines{lines: [[1]]} = input1
 
-    input2 = NestedLines.new!(["1", nil, "2"])
-    assert %NestedLines{lines: [[1], [1], [1]]} = input2
+      input2 = NestedLines.new!(["1", nil, "2"])
+      assert %NestedLines{lines: [[1], [1], [1]]} = input2
+    end
   end
 
-  test "[\"1\"] returns [[1]]" do
-    input = NestedLines.new!(["1"])
-    assert %NestedLines{lines: [[1]]} = input
+  describe "string values" do
+    test "[\"1\"] returns [[1]]" do
+      input = NestedLines.new!(["1"])
+      assert %NestedLines{lines: [[1]]} = input
+    end
+
+    test "[\"1\", \"2\"] returns [[1], [1]]" do
+      input = NestedLines.new!(["1", "2"])
+      assert %NestedLines{lines: [[1], [1]]} = input
+    end
+
+    test "[\"1\", \"1.1\", \"1.2\"] returns [[1], [0, 1], [0, 1]]" do
+      input = NestedLines.new!(["1", "1.1", "1.2"])
+      assert %NestedLines{lines: [[1], [0, 1], [0, 1]]} = input
+    end
   end
 
-  test "[\"1\", \"2\"] returns [[1], [1]]" do
-    input = NestedLines.new!(["1", "2"])
-    assert %NestedLines{lines: [[1], [1]]} = input
-  end
+  describe "numeric values" do
+    test "numeric values return [[1]]" do
+      input = NestedLines.new!([nil])
+      assert %NestedLines{lines: [[1]]} = input
 
-  test "[\"1\", \"1.1\", \"1.2\"] returns [[1], [0, 1], [0, 1]]" do
-    input = NestedLines.new!(["1", "1.1", "1.2"])
-    assert %NestedLines{lines: [[1], [0, 1], [0, 1]]} = input
-  end
+      input2 = NestedLines.new!(["1", 1, "2"])
+      assert %NestedLines{lines: [[1], [1], [1]]} = input2
 
-  test "numeric values return [[1]]" do
-    input = NestedLines.new!([nil])
-    assert %NestedLines{lines: [[1]]} = input
-
-    input2 = NestedLines.new!(["1", 1, "2"])
-    assert %NestedLines{lines: [[1], [1], [1]]} = input2
-
-    input3 = NestedLines.new!(["1", 1.1, "2", 2.1])
-    assert %NestedLines{lines: [[1], [0, 1], [1], [0, 1]]} = input3
+      input3 = NestedLines.new!(["1", 1.1, "2", 2.1])
+      assert %NestedLines{lines: [[1], [0, 1], [1], [0, 1]]} = input3
+    end
   end
 end
