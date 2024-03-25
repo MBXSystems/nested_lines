498734bc58dd9ec8a1847cdb8d9bf37a4cd0e0b8
Jesse Hixson <jesse.hixson@ahead.com>
2024-03-25 16:31:38 -0400

    Add ex_doc

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule NestedLines.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
     ]
   end
 
